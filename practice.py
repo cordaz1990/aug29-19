b3e044e2da2d307715f5e41cd67740f250ea3750
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:07:02 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,12 @@
-print('world')
-print('hello')
-print('world')
+class Time:
+  """ Represent the time of day """
+
+def print_time(time):
+    print('%.2d: %.2d:%.2d' % (time.hour, time.minute, time.second))
+    
+start = Time()
+start.hour = 9
+start. minute = 45
+start.second = 00
+print_time(start)
+09:45:00
